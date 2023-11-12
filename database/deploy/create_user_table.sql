1ecc8ded0ccbeca1310c9c62217cefc89ef92a4d
CharmFlex <lukjiaming@gmail.com>
2023-11-12 11:19:41 +0800

    add register user constrains

--- database/deploy/create_user_table.sql
+++ database/deploy/create_user_table.sql
@@ -5,8 +5,8 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS sportgether_schema.users(
     id bigserial PRIMARY KEY,
-    username text NOT NULL,
-    email text NOT NULL,
+    username text NOT NULL UNIQUE,
+    email text NOT NULL UNIQUE,
     password text NOT NULL,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     is_blocked bool NOT NULL DEFAULT false,
